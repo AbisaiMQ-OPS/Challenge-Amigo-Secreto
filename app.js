e330cfc1974e4ab4edd11369da3000ce9622bd77
AbisaiMQ <abisaimoraquezada@gmail.com>
2025-02-07 00:54:41 -0400

    Agregue condicional para la caja de nombres

---
+++ app.js
@@ -0,0 +1,19 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Crear una lista para almacenar los nombres
+let nombres = [];
+//Hacer una funcion que tome el nombre digitado lo almacene en la lista y lo muestre 
+function agregarAmigo() {
+   let nombreAmigo = document.getElementById('amigo');
+    if (nombreAmigo.innerText === "") {
+        alert('Necesitas agregar un nombre');
+        return
+    } 
+   // let nombre = l
+}
+// Generar un numero aleatorio
+//
+//
+//
+//
+//
